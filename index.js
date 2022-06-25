7c6e11a1e0b3b2818a92ce76b5d05277f8167670
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-25 12:42:20 +0300

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ function hasTargetSum(array, target) {
   }
   return false;
 }
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+}
+
+module.exports = hasTargetSum;
+module.exports = hasTargetSum; 
